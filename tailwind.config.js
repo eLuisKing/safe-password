a08fbac46366a232df3d2062c9f1c9cc626571f9
luisking96 <luistecisc@gmail.com>
2023-04-21 16:50:04 -0600

    appjs and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+				Sofia: ["Sofia Sans Condensed", "sans-serif"],
+			 }
+    },
   },
   plugins: [],
 }
