8f0d3fc0738ef917afe8017ab02149abc1ae69d2
luisking96 <luistecisc@gmail.com>
2023-04-21 22:41:05 -0600

    Adding nav movil/desk

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundColor: theme =>({
+				...theme('colors'),
+				'primary': '#06283D',
+        'secund': '#1363DF',
+        'third': '#47B5FF',
+        'fourth': '#DFF6FF'
+			}),
+			textColor:{
+				't-primary': '#1363DF'
+			},
       fontFamily: {
 				Sofia: ["Sofia Sans Condensed", "sans-serif"],
 			 }
