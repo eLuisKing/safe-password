cbe0340c7b103f6f996cad0f028a0ffbd7e0eb94
luisking96 <luistecisc@gmail.com>
2023-05-16 23:20:20 -0600

    Modal +

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,23 @@
-const btnAdd = document.getElementById('btn-add-movil');
+//Botones de los open/close modal
+const btnAddMovil = document.getElementById('btn-add-movil');
+const btnAddWeb = document.getElementById('btn-add-web');
 const hiddenMovilAdd = document.getElementById('generador-movil');
+const hiddenWebAdd = document.getElementById('modal-generador');
 
 
-
-btnAdd.addEventListener('click',function(){
+//Boton de open/close generador en movil
+btnAddMovil.addEventListener('click',function(){
     hiddenMovilAdd.classList.toggle('hidden');
+});
+
+//Boton de open/close generador en web modal
+btnAddWeb.addEventListener('click', function(){
+    hiddenWebAdd.classList.toggle('hidden');
+});
+
+//Aqui ocultamos el modal si la pantalla es menor a 800px(Movil)
+window.addEventListener('resize',function(){
+    if(innerWidth<800){
+        hiddenWebAdd.classList.add('hidden');
+    };
 });
